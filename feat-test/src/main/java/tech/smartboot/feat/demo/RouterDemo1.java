7ace36bf4ff1d0b49ac520d70a738216e13ac54c
zhengjw22 <zhengjunweimail@163.com>
2025-01-15 13:18:44 +0800

    更新文档

---
+++ feat-test/src/main/java/tech/smartboot/feat/demo/RouterDemo1.java
@@ -0,0 +1,19 @@
+package tech.smartboot.feat.demo;
+
+import tech.smartboot.feat.core.Feat;
+import tech.smartboot.feat.core.server.handler.Router;
+
+public class RouterDemo1 {
+    public static void main(String[] args) {
+        Router router = new Router();
+        router.route("/", (request) -> {
+            request.getResponse().write("root: " + request.getRequestURI());
+        });
+        router.route("/route1", (request) -> {
+            request.getResponse().write("route1: " + request.getRequestURI());
+        }).route("/route2", (request) -> {
+            request.getResponse().write("route2: " + request.getRequestURI());
+        });
+        Feat.createHttpServer().httpHandler(router).listen();
+    }
+}
