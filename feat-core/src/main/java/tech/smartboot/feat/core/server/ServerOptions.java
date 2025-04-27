84b352121968d408777198bca35d1ce624a2c081
zhengjw22 <zhengjunweimail@163.com>
2025-04-27 18:58:42 +0800

    v0.8.7

--- feat-core/src/main/java/tech/smartboot/feat/core/server/ServerOptions.java
+++ feat-core/src/main/java/tech/smartboot/feat/core/server/ServerOptions.java
@@ -29,7 +29,7 @@ import java.util.List;
  * @version v1.0.0
  */
 public class ServerOptions {
-    public static final String VERSION = "v0.8.6";
+    public static final String VERSION = "v0.8.7";
 
     /**
      * 缓存
