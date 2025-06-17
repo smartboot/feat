ba7f6a40cae87a5def936c1e58784dca9f27a5cd
zhengjw22 <zhengjunweimail@163.com>
2025-06-17 10:18:23 +0800

    feat(cloud): 增强 RequestMapping 注解功能并添加用户

--- feat-cloud/src/main/java/tech/smartboot/feat/cloud/annotation/RequestMapping.java
+++ feat-cloud/src/main/java/tech/smartboot/feat/cloud/annotation/RequestMapping.java
@@ -18,6 +18,26 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * <p>
+ * RequestMapping 注解用于将 HTTP 请求映射到特定的处理方法上。
+ * 它可以定义请求路径以及允许的请求方法类型。
+ * <p>
+ * 示例:
+ * <pre>
+ * {@code
+ * @Controller("/example")
+ * public class ExampleController {
+ *     @RequestMapping(value = "/get", method = RequestMethod.GET)
+ *     public String getExample() {
+ *         return "GET request received";
+ *     }
+ * }
+ * }
+ * </pre>
+ * <p>
+ * 在上面的例子中，`@RequestMapping("/example")` 设置了控制器的基础路径，
+ * `@RequestMapping(value = "/get", method = RequestMethod.GET)` 则指定了具体的 GET 请求路径。
+ *
  * @author 三刀(zhengjunweimail @ 163.com)
  * @version v1.0.0
  */
@@ -25,7 +45,19 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.SOURCE)
 @Documented
 public @interface RequestMapping {
+    /**
+     * 定义与该方法关联的 URL 路径。
+     * 默认为空字符串，意味着该方法可以通过基础路径访问。
+     *
+     * @return 请求路径，默认为空
+     */
     String value() default "";
 
+    /**
+     * 指定该方法支持的 HTTP 请求类型（如 GET, POST 等）。
+     * 默认为空数组，意味着不限制请求方法。
+     *
+     * @return 支持的请求方法数组，默认为空
+     */
     RequestMethod[] method() default {};
 }
