4b854a33be2aae642a4182a83324c13655871872
zhengjw22 <zhengjunweimail@163.com>
2025-06-25 11:18:16 +0800

    perf(core): 优化日志级别比较逻辑
    - 将字符串比较方式从 logger.getLevel().getName().equals(newLevel) 修改为 newLevel.equals(logger.getLevel().getName())
    - 这种修改可以避免潜在的 NullPointerException，并提高代码的健壮性

--- feat-core/src/main/java/tech/smartboot/feat/core/common/logging/RunLogger.java
+++ feat-core/src/main/java/tech/smartboot/feat/core/common/logging/RunLogger.java
@@ -74,7 +74,7 @@ class RunLogger implements tech.smartboot.feat.core.common.logging.Logger {
             if (FeatUtils.isBlank(newLevel)) {
                 newLevel = System.getProperty(LoggerFactory.SYSTEM_PROPERTY_LOG_LEVEL, "INFO");
             }
-            if (!logger.getLevel().getName().equals(newLevel)) {
+            if (!newLevel.equals(logger.getLevel().getName())) {
                 logger.setLevel(Level.parse(newLevel));
             }
             latestCheckTime = System.currentTimeMillis();
