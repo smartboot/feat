bffbac66ac1f72825569fc1b38a02a091dd9c58d
zhengjw22 <zhengjunweimail@163.com>
2025-08-19 19:18:06 +0800

    feat(pages): 添加全屏阅读模式并优化文档结构
    - 新增 starlight-fullview-mode 插件，实现全屏阅读功能
    - 调整内容模块加载顺序，优化文档结构
    - 更新 astro.config.mjs，配置全屏阅读模式插件
    - 在 package.json 和 package-lock.json 中添加 starlight-fullview-mode 依赖

--- pages/astro.config.mjs
+++ pages/astro.config.mjs
@@ -3,6 +3,8 @@ import {defineConfig} from 'astro/config';
 import starlight from '@astrojs/starlight';
 import starlightImageZoomPlugin from "starlight-image-zoom";
 import starlightScrollToTop from 'starlight-scroll-to-top';
+import starlightFullViewMode from 'starlight-fullview-mode'
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -63,7 +65,9 @@ export default defineConfig({
                 {icon: 'github', label: 'GitHub', href: 'https://github.com/smartboot/feat'},
                 {icon: 'seti:git', label: 'Gitee', href: 'https://gitee.com/smartboot/feat'}
             ],
-            plugins: [starlightImageZoomPlugin(),starlightScrollToTop({
+            plugins: [starlightImageZoomPlugin(),starlightFullViewMode({
+                leftSidebarEnabled: true,  rightSidebarEnabled: true
+            }),starlightScrollToTop({
                 // Button position
                 // Tooltip text
                 tooltipText: 'Back to top',
