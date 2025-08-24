e2b3a17d087748642009d493bd462f4342205910
zhengjw22 <zhengjunweimail@163.com>
2025-08-24 10:11:30 +0800

    build: 更新项目版本至 1.2.2
    - 修改了多个模块的 pom.xml 文件，将版本号从 1.2.1 改为 1.2.2
    - 更新了 Makefile 中的版本号
    -调整了 feat-core 中的版本常量

--- feat-core/src/main/java/tech/smartboot/feat/Feat.java
+++ feat-core/src/main/java/tech/smartboot/feat/Feat.java
@@ -47,7 +47,7 @@ public class Feat {
     /**
      * 当前 Feat 框架版本号
      */
-    public static final String VERSION = "v1.2.1";
+    public static final String VERSION = "v1.2.2";
 
     /**
      * 创建一个HTTP服务器实例，使用默认配置。
