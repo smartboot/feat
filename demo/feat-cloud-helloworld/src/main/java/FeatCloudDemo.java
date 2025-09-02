e735760d0590775afd695496790ecce88404fd0c
zhengjw22 <zhengjunweimail@163.com>
2025-09-02 10:20:36 +0800

    feat(demo): 添加 Feat Cloud 演示项目
    - 新增 feat-cloud-helloworld 项目，展示 Feat Cloud 的基本使用方法
    - 实现了一个简单的控制器，提供 "/cloud" 路径的请求处理
    - 配置 Maven 项目对象模型，包括所需的依赖和插件

---
+++ demo/feat-cloud-helloworld/src/main/java/FeatCloudDemo.java
@@ -0,0 +1,25 @@
+/*
+ *  Copyright (C) [2024] smartboot [zhengjunweimail@163.com]
+ *
+ *  企业用户未经smartboot组织特别许可，需遵循AGPL-3.0开源协议合理合法使用本项目。
+ *
+ *   Enterprise users are required to use this project reasonably
+ *   and legally in accordance with the AGPL-3.0 open source agreement
+ *  without special permission from the smartboot organization.
+ */
+
+import tech.smartboot.feat.cloud.FeatCloud;
+import tech.smartboot.feat.cloud.annotation.Controller;
+import tech.smartboot.feat.cloud.annotation.RequestMapping;
+
+@Controller
+public class FeatCloudDemo {
+    @RequestMapping("/cloud")
+    public String helloWorld() {
+        return "hello Feat Cloud";
+    }
+
+    public static void main(String[] args) {
+        FeatCloud.cloudServer().listen();
+    }
+}
