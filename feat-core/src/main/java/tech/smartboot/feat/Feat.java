e171cd4eccefd6587b3e1c5df8f4acb35a5247d3
zhengjw22 <zhengjunweimail@163.com>
2025-09-16 15:26:06 +0800

    build(feat): 更新项目版本至 1.3.1-SNAPSHOT
    - 将 feat-parent版本从 1.3.0 修改为 1.3.1-SNAPSHOT
    - 更新相关子模块版本
    - 修改 Feat.java 中的版本号为 v1.3.1-SNAPSHOT- 更新 Makefile 中的版本号
    - 修改 smartsocket 版本依赖至 1.7.0-SNAPSHOT

--- feat-core/src/main/java/tech/smartboot/feat/Feat.java
+++ feat-core/src/main/java/tech/smartboot/feat/Feat.java
@@ -42,7 +42,7 @@ public class Feat {
     /**
      * 当前 Feat 框架版本号
      */
-    public static final String VERSION = "v1.3.0";
+    public static final String VERSION = "v1.3.1-SNAPSHOT";
 
     /**
      * 创建一个HTTP服务器实例，使用默认配置。
