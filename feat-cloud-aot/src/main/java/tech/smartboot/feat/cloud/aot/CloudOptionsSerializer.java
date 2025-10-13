995c2df006ec7ea232257f88ccfc02828e890a4f
zhengjw22 <zhengjunweimail@163.com>
2025-10-13 09:46:22 +0800

    fix(cloud-aot):修复许可证序列化时缺少分号的问题
    - 在序列化 license_num时添加缺失的分号- 在序列化 license_name 时添加缺失的分号
    - 确保生成的代码语法正确，避免编译错误

--- feat-cloud-aot/src/main/java/tech/smartboot/feat/cloud/aot/CloudOptionsSerializer.java
+++ feat-cloud-aot/src/main/java/tech/smartboot/feat/cloud/aot/CloudOptionsSerializer.java
@@ -368,14 +368,15 @@ final class CloudOptionsSerializer implements Serializer {
             if (FeatUtils.isBlank(modelName)) {
                 printWriter.println("\t\tSystem.out.println(\"\\u001B[32mFeat License verification passed! License No: \\033[4m" + license.getNum() + "\\u001B[0m\\u001B[32m Granted for:\\033[4m" + license.getName() + "\\u001B[0m\");");
             } else {
-                printWriter.println("\t\tSystem.out.print(\"\\u001B[32mThe dependent module [\");");
-                printWriter.println("\t\tSystem.out.print(\"" + modelName + "\");");
-                printWriter.println("\t\tSystem.out.print(\"] has passed Feat License verification! License No: \\033[4m\\u001B[1m\");");
-                printWriter.println("\t\tSystem.out.print(\"" + license.getNum() + "\");");
-                printWriter.println("\t\tSystem.out.print(\"\\u001B[0m\\u001B[32m Granted for: \\033[4m\\u001B[1m\");");
-                printWriter.println("\t\tSystem.out.print(\"" + license.getName() + "\");");
-                printWriter.println("\t\tSystem.out.println(\"\\u001B[0m\");");
-//                printWriter.println("\t\tSystem.out.println(\"\\u001B[32mThe dependent module [" + modelName + "] has passed Feat License verification! License No:\\033[4m" + license.getNum() + "\\u001B[0m\\u001B[32m Granted for:\\033[4m" + license.getName() + "\\u001B[0m\");");
+                printWriter.println("\t\tStringBuilder sb = new StringBuilder();");
+                printWriter.println("\t\tsb.append(\"\\u001B[32mThe dependent module [\");");
+                printWriter.println("\t\tsb.append(\"" + modelName + "\");");
+                printWriter.println("\t\tsb.append(\"] has passed Feat License verification! License No: \\033[4m\\u001B[1m\");");
+                printWriter.println("\t\tsb.append(\"" + license.getNum() + "\");");
+                printWriter.println("\t\tsb.append(\"\\u001B[0m\\u001B[32m Granted for: \\033[4m\\u001B[1m\");");
+                printWriter.println("\t\tsb.append(\"" + license.getName() + "\");");
+                printWriter.println("\t\tsb.append(\"\\u001B[0m\");");
+                printWriter.println("\t\tSystem.out.println(sb.toString());");
             }
 
         }
