a22517622d741b58630ed7dbcb5580bdc6f053d4
zhengjw22 <zhengjunweimail@163.com>
2025-12-04 19:49:36 +0800

    feat(demo): 添加Bootstrap类并完善HelloWorld示例
    - 新增Bootstrap启动引导类，用于演示FeatCloud框架的基本用法
    - 在Bootstrap类中实现/hello路径的GET请求处理方法
    - 完善Bootstrap类的JavaDoc注释，包括类说明和方法说明
    - 在main方法中启动FeatCloud服务器
    - 移除pom.xml中不必要的maven.compiler.debug配置项

--- demo/helloworld/src/main/java/tech/smartboot/feat/Bootstrap.java
+++ demo/helloworld/src/main/java/tech/smartboot/feat/Bootstrap.java
@@ -14,14 +14,35 @@ import tech.smartboot.feat.cloud.FeatCloud;
 import tech.smartboot.feat.cloud.annotation.Controller;
 import tech.smartboot.feat.cloud.annotation.RequestMapping;
 
+/**
+ * Feat框架的启动引导类
+ * 该类演示了如何使用FeatCloud框架创建一个简单的Web应用程序
+ *
+ * @author smartboot
+ * @version 1.0
+ */
 @Controller
 public class Bootstrap {
+
+    /**
+     * 处理HTTP GET请求的控制器方法
+     * 当访问路径"/hello"时，会返回"hello Feat Cloud"字符串
+     *
+     * @return 响应字符串"hello Feat Cloud"
+     */
     @RequestMapping("/hello")
     public String helloWorld() {
         return "hello Feat Cloud";
     }
 
+    /**
+     * 程序入口点
+     * 启动FeatCloud服务器并开始监听请求
+     *
+     * @param args 命令行参数
+     */
     public static void main(String[] args) {
+        // 创建并启动FeatCloud服务器
         FeatCloud.cloudServer().listen();
     }
 }
