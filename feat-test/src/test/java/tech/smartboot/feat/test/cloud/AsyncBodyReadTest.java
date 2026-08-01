6a0ad32be093fd7842cd32444602a3287a28037c
luyao <luyao@tsign.cn>
2026-08-01 17:02:19 +0800

    优化单测

--- feat-test/src/test/java/tech/smartboot/feat/test/cloud/AsyncBodyReadTest.java
+++ feat-test/src/test/java/tech/smartboot/feat/test/cloud/AsyncBodyReadTest.java
@@ -1,6 +1,7 @@
 package tech.smartboot.feat.test.cloud;
 
 import com.alibaba.fastjson2.JSONObject;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,26 +11,28 @@ import tech.smartboot.feat.cloud.annotation.Param;
 import tech.smartboot.feat.cloud.annotation.RequestMapping;
 import tech.smartboot.feat.core.client.HttpClient;
 import tech.smartboot.feat.core.server.HttpRequest;
+import tech.smartboot.feat.core.server.HttpServer;
 import tech.smartboot.feat.test.BastTest;
 
 import java.util.concurrent.ExecutionException;
 
 @Controller("/asyncBody")
 public class AsyncBodyReadTest extends BastTest {
+    private HttpServer httpServer;
 
     @RequestMapping(value = "/small")
-    public String small(HttpRequest request,@Param("object") String object) {
+    public String small(HttpRequest request, @Param("object") String object) {
         return request.getContentLength() + "";
     }
 
     @RequestMapping(value = "/big")
-    public String big(HttpRequest request,@Param("object") String object) {
+    public String big(HttpRequest request, @Param("object") String object) {
         return request.getContentLength() + "";
     }
 
     @Before
     public void init() {
-        FeatCloud.cloudServer(opts -> opts.setPackages(AsyncBodyReadTest.class.getName()).readBufferSize(1024).debug(true)).listen(8080);
+        httpServer = FeatCloud.cloudServer(opts -> opts.setPackages(AsyncBodyReadTest.class.getName()).readBufferSize(1024).debug(true)).listen(8080);
     }
 
     @Test
@@ -40,12 +43,17 @@ public class AsyncBodyReadTest extends BastTest {
             jsonObject.put("key" + i, "value" + i);
         }
         int length = jsonObject.toString().getBytes().length;
-        Assert.assertEquals(httpClient.post("/asyncBody/small").postJson(jsonObject).submit().get().body(), length+"");
+        Assert.assertEquals(httpClient.post("/asyncBody/small").postJson(jsonObject).submit().get().body(), length + "");
 
         for (int i = 0; i < 1024; i++) {
             jsonObject.put("key" + i, "value" + i);
         }
         length = jsonObject.toString().getBytes().length;
-        Assert.assertEquals(httpClient.post("/asyncBody/big").postJson(jsonObject).submit().get().body(), length+"");
+        Assert.assertEquals(httpClient.post("/asyncBody/big").postJson(jsonObject).submit().get().body(), length + "");
+    }
+
+    @After
+    public void destroy() {
+        httpServer.shutdown();
     }
 }
